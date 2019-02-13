7472f989c60c3d542e9bf71813ea6b790b4f56bc
Jaydeep Dholakia <jaydeep.k.dholakia@gmail.com>
2019-02-13 12:56:31 +0530

    Updating main.py for the chatbot

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def check_balance(req):
 
                 for row in records:
                     bal = row[0]
-                    bal = 'Your Savings balance is:', bal
+                    bal = "Your Savings balance is:", bal
                     bal = str(bal)
 
             except Error as e:
