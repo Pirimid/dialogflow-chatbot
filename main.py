536a50f28aaea4bc329db82003dc7b2e9d8d5364
Jaydeep Dholakia <jaydeep.k.dholakia@gmail.com>
2019-02-14 14:11:07 +0530

    testing Transaction part in main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def webhook():
     This is meant to be used in conjunction with the Banking Dialogflow agent
     """
     req = request.get_json(silent=True, force=True)
-    print(req)
     try:
         action = req.get('result').get('action')
     except AttributeError:
@@ -22,10 +21,10 @@ def webhook():
 
     if action == 'check_balance':
         res = check_balance(req)
-        
 
     elif action == 'get_transactions':
-        res = get_transaction(req)
+        res = get_transactions(req)
+
     
     return make_response(jsonify({"speech": res}))
 
@@ -72,37 +71,10 @@ def MySQL(querry):
             mySQLconnection.close()
             print("MySQL connection is closed")
 
-def get_transaction(req):
-    parameters = req['result']['parameters']
-
-   if parameters.get('branch'):
+def get_transactions(req):
     records = MySQL("select * from transaction order by TranscationDate DESC LIMIT 10 ;")
     for row in records:
-        return 'TransactionID: %s' %row[0]
-        return 'AccountID: '% row[1]
-        return 'Credit: ' %row[2]
-        return 'Debit: ' %row[3]
-        return 'balance: ' %row[4]
-        return 'TransactionType: ' %row[5]
-        return 'TranscationDate: ' %row[6] '\n'    
-            """ 
-           if str(parameters.get('branch')) == str('branch city'):
-               records = MySQL("select Balance from account where AccountType='Savings';'")
-               for row in records:
-                   bal = row[0]
-                   return 'Your Savings balance is: %s' % bal
-   
-           elif str(parameters.get('branch')) == str('branch area'):
-               records = MySQL("select Balance from account where AccountType='Current';'")
-               for row in records:
-                   bal = row[0]
-                   return 'Your Current balance is: %s' % bal
-   
-           elif str(parameters.get('branch')) == str('branch name'):
-               records = MySQL("select Balance from account where AccountType='Current';'")
-   
-           elif str(parameters.get('branch')) == str('branch information'):
-               records = MySQL("select Balance from account where AccountType='Current';'") """   
+        return 'TransactionID: %s ' %row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6] + '\n'
 
 
 if __name__ == '__main__':
