263ebf0f5747b317fa30a64f19b05332f3b3851d
Jaydeep Dholakia <jaydeep.k.dholakia@gmail.com>
2019-02-14 13:03:57 +0530

    testing Transaction part in main.py

--- main.py
+++ main.py
@@ -22,53 +22,12 @@ def webhook():
 
     if action == 'check_balance':
         res = check_balance(req)
-        return make_response(jsonify({"speech": res}))
+        
 
-"""    if parameters.get('account'):
-        if str(parameters.get('account')) == str('savings'):
-            try:
-                mySQLconnection = mysql.connector.connect(host='203.88.129.243',
-                    database='banking',
-                    user='jaydeep',
-                    password='jaydeep',
-                    port='1234')
-                sql_select_Query = "select balance from account where account_type='savings'"
-                cursor = mySQLconnection .cursor()
-                cursor.execute(sql_select_Query)
-                records = cursor.fetchall()
-
-                for row in records:
-                    ful = row[0]
-                    ful_send = str(ful)
-                    print("Your savings balance is = ", row[0])
-            except Error as e:
-                print ("Error while connecting to MySQL", e)
-            finally:
-                if(mySQLconnection .is_connected()):
-                    mySQLconnection.close()
-
-        if parameters.get('account') == 'current':
-            try:
-                mySQLconnection = mysql.connector.connect(host='203.88.129.243',
-                    database='banking',
-                    user='jaydeep',
-                    password='jaydeep',
-                    port='1234')
-                sql_select_Query = "select balance from account where account_type='current'"
-                cursor = mySQLconnection .cursor()
-                cursor.execute(sql_select_Query)
-                records = cursor.fetchall()
-                
-                for row in records:
-                    response = row[0]
-                    print("Your current balance is = ", row[0])
-            except Error as e:
-                print ("Error while connecting to MySQL", e)
-            finally:
-                if(mySQLconnection .is_connected()):
-                    mySQLconnection.close()
-                    print("MySQL connection is closed")"""
+    elif action == 'get_transactions':
+        res = get_transaction(req)
     
+    return make_response(jsonify({"speech": res}))
 
 
 def check_balance(req):
@@ -86,7 +45,7 @@ def check_balance(req):
                 return 'Your Savings balance is: %s' % bal
 
         elif str(parameters.get('account')) == str('current'):
-            records = MySQL("select Balance from account where AccountType='Current';'")
+            records = MySQL("select Balance from account where AccountType='Current';")
 
             for row in records:
                 bal = row[0]
@@ -112,6 +71,38 @@ def MySQL(querry):
         if(mySQLconnection .is_connected()):
             mySQLconnection.close()
             print("MySQL connection is closed")
+
+def get_transaction(req):
+    parameters = req['result']['parameters']
+
+   if parameters.get('branch'):
+    records = MySQL("select * from transaction order by TranscationDate DESC LIMIT 10 ;")
+    for row in records:
+        return 'TransactionID: %s' %row[0]
+        return 'AccountID: '% row[1]
+        return 'Credit: ' %row[2]
+        return 'Debit: ' %row[3]
+        return 'balance: ' %row[4]
+        return 'TransactionType: ' %row[5]
+        return 'TranscationDate: ' %row[6] '\n'    
+            """ 
+           if str(parameters.get('branch')) == str('branch city'):
+               records = MySQL("select Balance from account where AccountType='Savings';'")
+               for row in records:
+                   bal = row[0]
+                   return 'Your Savings balance is: %s' % bal
+   
+           elif str(parameters.get('branch')) == str('branch area'):
+               records = MySQL("select Balance from account where AccountType='Current';'")
+               for row in records:
+                   bal = row[0]
+                   return 'Your Current balance is: %s' % bal
+   
+           elif str(parameters.get('branch')) == str('branch name'):
+               records = MySQL("select Balance from account where AccountType='Current';'")
+   
+           elif str(parameters.get('branch')) == str('branch information'):
+               records = MySQL("select Balance from account where AccountType='Current';'") """   
 
 
 if __name__ == '__main__':
