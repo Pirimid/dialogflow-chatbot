8212341d4dd2d615f30a7cea25d306c9c3a37975
Jaydeep Dholakia <jaydeep.k.dholakia@gmail.com>
2019-02-18 14:21:49 +0530

    Adding code for transaction's parameters

--- main.py
+++ main.py
@@ -73,35 +73,43 @@ def MySQL(querry):
 def get_transactions(req):
     parameters = req['result']['parameters']
     if parameters.get('transaction') or parameters.get('last') or parameters.get('date') or parameters.get('number') or parameters.get('date-period'):
+        if parameters.get('last') and parameters.get('number'):
+            number_days = parameters.get('number')
+            querry_pre = "select Credit, Debit, Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y') from transaction order by TranscationDate DESC LIMIT {};".format(number_days)
+            records = MySQL(querry_pre)
+            st = ''
+            for row in records:
+                    st = st + 'Transaction was performed on %s'%row[3]+' with Credit: %s'%row[0]+', Debit: %s'%row[1]+' and Balance after that was: %s'%row[2]+ "\n" 
+                    print(st)
+            return st
+
         if parameters.get('transaction') and parameters.get('last'):
             type_of_transaction = parameters.get('transaction')
             querry_pre = "select Credit, Debit, Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y') from transaction where(TransactionType = '{}') order by TranscationDate DESC LIMIT 1;".format(type_of_transaction)
             records = MySQL(querry_pre)
             st = ''
             for row in records:
-                    st = st + 'Your last %s'%type_of_transaction+' was performed on %s'%row[3]+' with Credit: %s'%row[0]+', Debit: %s'%row[1]+' and Balance was after that: %s'%row[2]+ "\n " 
+                    st = st + 'Your last %s'%type_of_transaction+' was performed on %s'%row[3]+' with Credit: %s'%row[0]+', Debit: %s'%row[1]+' and Balance after that was: %s'%row[2]+ "\n" 
                     print(st)
             return st
 
-        if parameters.get('last'):
+        elif parameters.get('last'):
             querry_pre = "select Credit, Debit, Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y') from transaction order by TranscationDate DESC LIMIT 1;"
             records = MySQL(querry_pre)
             st = ''
             for row in records:
-                    st = st + 'Your last transaction was performed on %s'%row[3]+' with Credit: %s'%row[0]+', Debit: %s'%row[1]+' and Balance was after that: %s'%row[2]+ "\n " 
+                    st = st + 'Your last transaction was performed on %s'%row[3]+' with Credit: %s'%row[0]+', Debit: %s'%row[1]+' and Balance after that was: %s'%row[2]+ "\n" 
                     print(st)
-            return st            
+            return st
+
     else:
         records = MySQL("select *,DATE_FORMAT(TranscationDate, '%m/%d/%Y') from transaction order by TranscationDate DESC LIMIT 10 ;")
         st = ''
         for row in records:
-            st = st + 'You had performed %s ' %row[5] + 'Type of Transaction On %s ' %row[7] + 'And it was of Credit: %s ' %row[2] + '& Debit: %s ' %row[3] + ', after which the balance was: %s ' %row[4] + "\n " 
+            st = st + 'You had performed %s ' %row[5] + 'Type of Transaction On %s ' %row[7] + 'And it was of Credit: %s ' %row[2] + '& Debit: %s ' %row[3] + ', after which the balance was: %s ' %row[4] + "\n" 
             #TransactionID: %s '%row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6]
             print(st)
         return st
-    
-def transactionsLoop(row):
-    return 'TransactionID: %s'%row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6]
 
 if __name__ == '__main__':
     app.run()
