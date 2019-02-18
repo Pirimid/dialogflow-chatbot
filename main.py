841daae1ac6d4a90b44c21969b7cf6bb4996d6d0
Jaydeep Dholakia <jaydeep.k.dholakia@gmail.com>
2019-02-18 12:40:47 +0530

    Adding code for transaction's parameters

--- main.py
+++ main.py
@@ -22,11 +22,6 @@ def webhook():
         res = check_balance(req)
 
     elif action == 'get_transactions':
-        parameters = req['result']['parameters']
-
-        if parameters.get('account'):
-            pass
-
         res = get_transactions(req)
 
     
@@ -76,13 +71,26 @@ def MySQL(querry):
             print("MySQL connection is closed")
 
 def get_transactions(req):
-    records = MySQL("select *,DATE_FORMAT(TranscationDate, '%m/%d/%Y') from transaction order by TranscationDate DESC LIMIT 10 ;")
-    st = ''
-    for row in records:
-        st = st + 'You had performed %s ' %row[5] + 'Type of Transaction On %s ' %row[7] + 'And it was of Credit: %s ' %row[2] + '& Debit: %s ' %row[3] + ', after which the balance was: %s ' %row[4] + "\n " 
-        #TransactionID: %s '%row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6]
-        print(st)
-    return st
+    parameters = req['result']['parameters']
+    if parameters.get('transaction') or parameters.get('last') or parameters.get('date') or parameters.get('number') or parameters.get('date-period'):
+        if parameters.get('transaction') and parameters.get('last'):
+            type_of_transaction = parameters.get('transaction')
+            querry_pre = "select Credit, Debit, Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y') from transaction where(TransactionType = '{}') order by TranscationDate DESC LIMIT 1;".format(type_of_transaction)
+            records = MySQL(querry_pre)
+            st = ''
+            for row in records:
+                    st = st + 'Your last %s'%type_of_transaction+' was performed on %s'%row[3]+' with Credit: %s'%row[0]+', Debit: %s'%row[1]+' and Balance was after that: %s'%row[2]+ "\n " 
+                    #TransactionID: %s '%row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6]
+                    print(st)
+            return st
+        else:
+            records = MySQL("select *,DATE_FORMAT(TranscationDate, '%m/%d/%Y') from transaction order by TranscationDate DESC LIMIT 10 ;")
+            st = ''
+            for row in records:
+                st = st + 'You had performed %s ' %row[5] + 'Type of Transaction On %s ' %row[7] + 'And it was of Credit: %s ' %row[2] + '& Debit: %s ' %row[3] + ', after which the balance was: %s ' %row[4] + "\n " 
+                #TransactionID: %s '%row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6]
+                print(st)
+            return st
     
 def transactionsLoop(row):
     return 'TransactionID: %s'%row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6]
