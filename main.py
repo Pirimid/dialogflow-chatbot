477c61039128d50c1b9131aeae72e7deac559bd5
Jaydeep Dholakia <jaydeep.k.dholakia@gmail.com>
2019-04-11 15:08:53 +0530

    Updated the Loan Use case

--- main.py
+++ main.py
@@ -33,6 +33,9 @@ def webhook():
     elif action == 'loan_eligibility':
         res = loan_eligibil(req)
 
+    elif action == 'account.spending.check':
+        res = account_spending(req)
+
     return make_response(jsonify({"speech": res}))
 
 def check_balance(req):
@@ -149,65 +152,6 @@ def get_transactions(req):
             st = st + 'Date: %s'%row[4]+', Credit: %s'%row[1]+' of: %s'%row[5]+' from %s'%row[0] + "\n"
     return st
 
-"""    if parameters.get('transaction') or parameters.get('last') or parameters.get('date') or parameters.get('number') or 
-    parameters.get('date-period'):
-        if parameters.get('number'):
-            number_days = parameters.get('number')
-            querry_pre = "select account.AccountType, Credit, Debit, transaction.Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y'), description from transaction inner join Account on transaction.AccountID = account.AccountID order by TranscationDate DESC LIMIT {};".format(number_days)
-            records = MySQL(querry_pre)
-            st = ''
-            for row in records:
-                if row[1]==0:
-                    st = st + 'Date: %s'%row[4]+', Debit: %s'%row[2]+' of: %s'%row[5]+' from %s'%row[0] + "\n"
-                else:
-                    st = st + 'Date: %s'%row[4]+', Credit: %s'%row[1]+' of: %s'%row[5]+' from %s'%row[0] + "\n"
-            return st
-
-        if parameters.get('transaction') and parameters.get('last'):
-            type_of_transaction = parameters.get('transaction')
-            querry_pre = "select account.AccountType, Credit, Debit, transaction.Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y'), description from transaction inner join Account on transaction.AccountID = account.AccountID where(TransactionType = '{}') order by TranscationDate DESC LIMIT 1;".format(type_of_transaction)
-            records = MySQL(querry_pre)
-            st = ''
-            for row in records:
-                if row[1]==0:
-                    st = st + 'Last %s'%type_of_transaction+' was on %s'%row[4]+' with Debit: %s'%row[2]+' of: %s'%row[5]+' from %s'%row[0]+ "\n"
-                else:
-                    st = st + 'Last %s'%type_of_transaction+' was on %s'%row[4]+' with Credit: %s'%row[1]+'of: %s'%row[5]+' from %s'%row[0]+ "\n"
-            return st
-
-        elif parameters.get('last'):
-            querry_pre = "select account.AccountType, Credit, Debit, transaction.Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y'), description from transaction inner join Account on transaction.AccountID = account.AccountID order by TranscationDate DESC LIMIT 1;"
-            records = MySQL(querry_pre)
-            st = ''
-            for row in records:
-                if row[1]==0:
-                    st = st + 'Your last transaction was performed on %s'%row[4]+' with Debit: %s'%row[2]+' of: %s'%row[5]+' from %s'%row[0]+ "\n"
-                else:
-                    st = st + 'Your last transaction was performed on %s'%row[4]+' with Credit: %s'%row[1]+' of: %s'%row[5]+' from %s'%row[0]+ "\n"
-            return st
-
-    else:
-        records = MySQL("select account.AccountType, Credit, Debit, transaction.TransactionType, transaction.Balance, DATE_FORMAT(TranscationDate, '%m/%d/%Y'), description from transaction inner join Account on transaction.AccountID = account.AccountID order by TranscationDate DESC LIMIT 5;")
-        st = ''
-        for row in records:
-            if row[1]==0:
-                st = st + 'Type: %s ' %row[3] + ', Date: %s ' %row[5] + ', Debit: %s ' %row[2] + ', of: %s'%row[6]+' From: %s'%row[0] + "\n"
-            else: 
-                st = st + 'Type: %s ' %row[3] + ', Date: %s ' %row[5] + ', Credit: %s ' %row[1] + ', of: %s'%row[6]+' From: %s'%row[0] + "\n"
-            #TransactionID: %s '%row[0] + 'AccountID: %s '% row[1] + 'Credit: %s ' %row[2] + 'Debit: %s ' %row[3] + 'balance: %s ' %row[4] + 'TransactionType: %s ' %row[5] + 'TranscationDate: %s' %row[6]
-        return st"""
-
-"""    records = MySQL("select DATE_FORMAT(TranscationDate, '%m/%d/%Y'), Balance from transaction where(AccountID = '1') order by TranscationDate DESC;")
-    data = records
-    dates = []
-    values = []
-    for row in data:
-        dates.append(parser.parse(row[0]))
-        values.append(row[1])
-
-    plt.plot_date(dates,values,'-')
-    plt.show()"""
-
 def get_cibilscore(req):
     records = MySQL("select score from cibil;")
     global cibil
@@ -218,10 +162,39 @@ def get_cibilscore(req):
 def loan_eligibil(req):
     parameters = req['result']['parameters']
     res = get_cibilscore(req)
-    type_of_loan = "Loan" if parameters.get('type_of_loan')=='' else parameters.get('type_of_loan')
-    if cibil<650:
+    type_of_loan = parameters.get('type_of_loan')
+    if type_of_loan == 'Student loan':
+        res = 'You can get {}, you will be contacted with an bank representative soon'.format(type_of_loan,cibil)
+    elif cibil<650:
         res = 'So for {}, you are not eligible as your CIBIL score is: {} which is low for granting you a {}'.format(type_of_loan,cibil,type_of_loan)
     return res
+
+def account_spending(req):
+    parameters = req['result']['parameters']
+    date_now = datetime.datetime.now()
+    merchant_name = parameters.get('merchant')
+    category = parameters.get('category')
+    accType_tran = "('savings'and'current')" if parameters.get('account')=='' else parameters.get('account')
+    if parameters.get('account'):
+        accType_tran = "'%s'"%accType_tran
+
+    if parameters.get('date'):
+        date_tran = parameters.get('date')
+        year,month,day = date_tran.split('-', 3)
+        year = int(year)
+        month = int(month)
+        day = int(day)
+        start_date = datetime.datetime(year,month,1).strftime("%Y-%m-%d")
+        end_date = datetime.datetime(year,month,calendar.mdays[month]).strftime("%Y-%m-%d")
+        print(start_date,end_date)
+    else:
+        datePeriod_tran = date_now.strftime("%m-%d-%Y") if parameters.get('date-period')=='' else parameters.get('date-period')
+        if datePeriod_tran != parameters.get('date-period'):
+            start_date = datetime.datetime(date_now.year,1,1).strftime("%Y-%m-%d")
+            end_date = datetime.datetime(date_now.year,date_now.month,calendar.mdays[date_now.month]).strftime("%Y-%m-%d")
+        else:
+            start_date,end_date = datePeriod_tran.split('/', 1)
+
 
 if __name__ == '__main__':
     app.run()
